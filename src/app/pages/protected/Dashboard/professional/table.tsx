d0878ca283170e759d7ee85d7ae03a45e0b23caa
rocio.malan <rocio.malan@uap.edu.ar>
2024-12-06 20:12:36 -0300

    new branch

--- src/app/pages/protected/Dashboard/professional/table.tsx
+++ src/app/pages/protected/Dashboard/professional/table.tsx
@@ -9,11 +9,66 @@ import {
     TableCell,
   } from "@/components/ui/table";
 
-  
+interface Turnos {
+
+} 
 
 
   
   const TurnoTable = () => {
+    const [turnos, setTurnos] = useState<Turnos[]>([]);
+    const [loading, setLoading] = useState (true);
+    const [error, setError] = useState<string | null>(null);
+    const { data: session } = useSession();
+
+    useEffect(() => {
+        const fetchTurnos = async () => {
+            if (!session?.user?.email) {
+                setError("No se encontró el email del usuario.");
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const email = session.user.email;
+
+                const response = await fetch(
+                    `http://localhost:3000/api/getAllAppointments?email=${email}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    throw new Error(errorData.error || "Error al cargar los datos");
+                }
+
+                const data = await response.json();
+                console.log(data);
+                setTurnos(data);
+                setError(null);
+            } catch (err: any) {
+                console.error("Error al cargar reservaciones:", err);
+                setError(err.message || "Error desconocido");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchTurnos();
+    }, [session?.user?.email]);
+
+    if (loading) {
+        return <div className="text-center">Cargando...</div>;
+    }
+
+    if (error) {
+        return <div className="text-center text-red-500">Error: {error}</div>;
+    }
      
   
       return (
