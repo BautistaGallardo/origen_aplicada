4c0742f2941823de05544ebf38e71e56b470fb93
Bautista Gallardo <bautistaggallardo@gmail.com>
2024-12-07 20:23:10 -0300

    fix

--- src/components/landing/conctactUs.tsx
+++ src/components/landing/conctactUs.tsx
@@ -4,7 +4,6 @@ import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
-import { ContactEmail } from '@/libs/zod';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
